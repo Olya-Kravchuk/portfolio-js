977221d969944f0f3346f49e3641fd955dccaa97
Olya-Kravchuk <143799228+Olya-Kravchuk@users.noreply.github.com>
2025-03-16 14:24:38 +0100

    sidebar

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const video1 = document.getElementById("projectvideo1");
 const video2 = document.getElementById("projectvideo2");
 const video3 = document.getElementById("projectvideo3");
 
+const sidebar = document.querySelector(".sidebar");
+const menu = document.querySelector(".menu-icon");
+const closeBtn = document.querySelector(".close-icon");
 
 const videoList = [video1, video2, video3];
 
@@ -13,3 +16,13 @@ videoList.forEach(function(video){
         video.pause()
     })
 })
+
+menu.addEventListener("click", function(){
+    sidebar.classList.remove("close-sidebar")
+    sidebar.classList.add("open-sidebar")
+})
+
+closeBtn.addEventListener("click", function () {
+    sidebar.classList.remove("open-sidebar")
+    sidebar.classList.add("close-sidebar")
+});
